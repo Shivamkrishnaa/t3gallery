3eaead20076a12e5d5042547edb7c22a87bb8a45
SHIVAM KRISHNA <30428839+Shivamkrishnaa@users.noreply.github.com>
2024-04-26 03:56:00 +0530

    use next images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/db/queries";
 export const dynamic = "force-dynamic";
 
 async function Images() {
   const images = await getMyImages();
-  return (<div className="flex flex-wrap gap-4">
+  return (<div className="flex flex-wrap justify-center gap-4">
     {images.map(image => (
       <div key={image.id} className="w-48 p-4">
-        <img alt={image.name} src={image.url} />
+
+        <Image width={500} height={500} alt={image.name} style={{objectFit: "contain"}} src={image.url} />
         <div>{image.name}</div>
       </div>
     ))}
